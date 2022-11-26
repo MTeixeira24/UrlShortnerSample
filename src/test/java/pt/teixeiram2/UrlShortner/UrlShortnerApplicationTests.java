ece093a53c0635e09ac8b5329f1548d6cd9ec84d
MTeixeira24 <miguelangelo_teixeira@hotmail.com>
2022-11-26 22:00:09 +0000

    Initial Commit

---
+++ src/test/java/pt/teixeiram2/UrlShortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.teixeiram2.UrlShortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
